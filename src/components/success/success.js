b540800e6724c9db3c0501d06bbd01f00e5bf3e1
theBeginner86 <pranavsingh02@hotmail.com>
2021-10-02 08:53:38 +0530

    Add success component

---
+++ src/components/success/success.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { HashLink as Link } from 'react-router-hash-link';
+import { useHistory } from 'react-router-dom';
+import './styles.css'
+
+function Success(){
+
+    const history = useHistory();
+
+    function handleSubmit(){
+        history.push('/');
+    }
+
+    return (
+        <div className="s-umbotron">
+            <div className="container" Style={"padding-top:5rem;"}>
+
+            <h1 className="display-4">SUCCESS</h1>
+            <p className="lead">Thanks for signing up. Have a good day!!.</p>
+
+            <form onSubmit={handleSubmit}>
+                <button type="submit" className="btn btn-warning">Go Back</button>
+            </form>
+            
+            </div>
+      </div>
+    );
+}
+
+export default Success;
