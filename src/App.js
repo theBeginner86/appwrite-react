a03370135cc91d2a4c03bc1e27cc76e8fe6cefba
theBeginner86 <pranavsingh02@hotmail.com>
2021-10-02 08:54:39 +0530

    Import success and failure components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import LoginUser from "./components/inputForm";
+import LoginUser from "./components/inputForm/inputForm";
+import Success from './components/success/success';
+import Failure from './components/failure/failure';
 
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route path='/signup' component={LoginUser} />
+        <Route path='/' component={LoginUser} exact/>
+        <Route path='/success' component={Success} />
+        <Route path='/failure' component={Failure} />
       </Switch>
     </Router>
   );    
